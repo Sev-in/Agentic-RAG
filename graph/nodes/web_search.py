338ef38e5d94a392ca86cf030d96b72b81b245a0
Şevin Sönmez <sevinsonmezz@gmail.com>
2026-01-23 17:42:39 +0300

    Created web_search.py

---
+++ graph/nodes/web_search.py
@@ -0,0 +1,28 @@
+from typing import Any,Dict
+from langchain_core.documents import Document
+from graph.state import GraphState
+from langchain_community.tools.tavily_search import TavilySearchResults
+from dotenv import load_dotenv
+
+load_dotenv()
+
+web_search_tool = TavilySearchResults(k=3)
+
+def web_search(state: GraphState) -> Dict[str, Any]:
+    print("-----WEB SEARCH-----")
+
+    question = state["question"]
+    documents = state["documents"]
+
+    tavily_results = web_search_tool.invoke({"query":question})
+    joined_tavily_result = "\n".join(
+        [tavily_result["content"] for tavily_result in tavily_results]
+    )
+
+    web_results = Document(page_content=joined_tavily_result,metadata={"source": "web", "tool": "tavily"})
+    if documents is not None:
+        documents.append(web_results)
+    else:
+        documents = [web_results]
+
+    return {"documents": documents, "question": question}
